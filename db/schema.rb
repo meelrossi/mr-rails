2272686204a76f31524c6bdfdb2a7380f5a65e3c
Melisa Rossi <melisa.rossi@wolox.com.ar>
2017-05-24 16:44:28 -0300

    added book suggestion model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170524170943) do
+ActiveRecord::Schema.define(version: 20170524193829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,20 @@ ActiveRecord::Schema.define(version: 20170524170943) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "book_suggestions", force: :cascade do |t|
+    t.string   "editorial"
+    t.float    "price"
+    t.string   "author"
+    t.string   "title"
+    t.string   "link"
+    t.string   "publisher"
+    t.string   "year"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_book_suggestions_on_user_id", using: :btree
   end
 
   create_table "books", force: :cascade do |t|
@@ -89,6 +103,7 @@ ActiveRecord::Schema.define(version: 20170524170943) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "book_suggestions", "users"
   add_foreign_key "rents", "books"
   add_foreign_key "rents", "users"
 end
