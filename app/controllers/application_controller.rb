29efbc020c02b7b821f90bb4bba975e79902c487
Melisa Rossi <melisa.rossi@wolox.com.ar>
2017-06-21 12:40:29 -0300

    use current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   include Wor::Authentication::Controller
   before_action :authenticate_request
   protect_from_forgery with: :null_session
-
-  MIN_PAGE_SIZE = 1
-  MAX_PAGE_SIZE = 100
+  attr_reader :current_user
 
   rescue_from Wor::Authentication::Exceptions::NotRenewableTokenError,
               with: :render_not_renewable_token
@@ -27,7 +25,7 @@ class ApplicationController < ActionController::Base
   end
 
   def find_authenticable_entity(entity_payload_returned_object)
-    User.find_by(email: entity_payload_returned_object.fetch(ENTITY_KEY))
+    @current_user ||= User.find_by(email: entity_payload_returned_object.fetch(ENTITY_KEY))
   end
 
   def entity_custom_validation_value(entity)
@@ -37,15 +35,5 @@ class ApplicationController < ActionController::Base
   def entity_custom_validation_invalidate_all_value(entity)
     entity.trackable_value = Devise.friendly_token
     entity.save
-  end
-
-  def paginate(rel)
-    page = params[:page].to_i
-    limit = params[:limit].to_i
-    limit.between?(MIN_PAGE_SIZE, MAX_PAGE_SIZE) || limit = MAX_PAGE_SIZE
-    values = rel.limit(limit).offset(page * limit)
-    { values: ActiveModelSerializers::SerializableResource.new(values),
-      page: page,
-      limit: limit }
   end
 end
